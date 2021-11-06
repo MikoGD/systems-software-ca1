c80e4b48f89a6336db1116d8d1fe3e5e692557db
Mikael Escolin <mikaelescolin@gmail.com>
2021-11-06 13:34:43 +0000

    Add function to return date in system format

--- src/utils.c
+++ src/utils.c
@@ -4,6 +4,17 @@
 
 #define TIME_STR_LENGTH 50
 
+char *get_local_date_str_system_format()
+{
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  char *time_str = (char *)calloc(TIME_STR_LENGTH, sizeof(char));
+
+  sprintf(time_str, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900 - 2000);
+
+  return time_str;
+}
+
 char *get_local_datetime_str()
 {
   time_t t = time(NULL);
