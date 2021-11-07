5a02400501c4a6900ddb7033721a239e8391ce37
Mikael Escolin <mikaelescolin@gmail.com>
2021-11-07 16:34:12 +0000

    Update report creation to have have logs

--- src/report.c
+++ src/report.c
@@ -1,6 +1,8 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <syslog.h>
 #include <unistd.h>
 #include "utils.h"
 
@@ -15,13 +17,28 @@ void create_aureport(char *key, char *report_folder_path)
   size_t datetime_length = strlen(datetime);
   size_t report_name_length = strlen(report_name);
 
-  char *execute = (char *)calloc(command_length + key_length + report_folder_path_length + datetime_length + report_name_length, sizeof(char));
-  char *full_report_path = (char *)calloc(report_name_length + datetime_length + report_folder_path_length, sizeof(char));
+  char *execute = (char *)calloc(command_length + key_length + report_folder_path_length + datetime_length +
+                                     report_name_length,
+                                 sizeof(char));
+
+  char *full_report_path = (char *)calloc(report_name_length + datetime_length + report_folder_path_length,
+                                          sizeof(char));
 
   sprintf(full_report_path, report_name, report_folder_path, datetime);
   sprintf(execute, command, key, full_report_path);
 
-  printf("execute: %s\n", execute);
+  int status = system(execute);
 
-  system(execute);
+  openlog("file management", LOG_PID | LOG_CONS, LOG_DAEMON);
+
+  if (status < 0)
+  {
+    syslog(LOG_ERR, "error creating aureport: %s", strerror(errno));
+  }
+  else if (WIFEXITED(status))
+  {
+    syslog(LOG_INFO, "exiting creating aureport with status %d", WEXITSTATUS(status));
+  }
+
+  closelog();
 }
