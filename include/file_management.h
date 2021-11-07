f1fe595fc0a2633867c3d420c029de360c39cda4
Mikael Escolin <mikaelescolin@gmail.com>
2021-11-07 17:06:22 +0000

    Remove return file management

--- include/file_management.h
+++ include/file_management.h
@@ -2,8 +2,8 @@
 
 #define FILE_MANAGEMENT
 
-int transfer_files(char *dest_path, char *src_path);
-int backup_folder(char *backup_path, char *folder_to_backup_path);
+void transfer_files(char *dest_path, char *src_path);
+void backup_folder(char *backup_path, char *folder_to_backup_path);
 void check_for_empty_folders(char *folder_path);
 
 #endif
